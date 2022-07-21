e13024b4b1a3d858c8c9791ab23ac4643701b01d
Alex <qwq199313@gmail.com>
2022-07-21 17:36:54 +0300

    upload for further upload on pythonanywhere

---
+++ README.md
@@ -0,0 +1,13 @@
+# Educational project in Django
+A site that I made, repeating after the course teacher. Posted on github as a way to upload to the server for further learning how to work with the server.
+
+### Project structure
+The structure is standard for the Django framework.
+
+### Hosting
+I plan to upload the site to the hosting pythonanywhere.com.
+
+### Site appearance
+Screenshot of one of the pages of the site:
+<p><img src="screenshot.png width=800"></p>
+   
---
+++ coolsite/women/templatetags/women_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from women.models import *
+from women.views import menu
+
+register = template.Library()
+
+
+@register.simple_tag(name='getcats')
+def get_categories(filter=None):
+    if filter is None:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(sort=None, cat_selected=0):
+    if sort is None:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+
+    return {"cats": cats, "cat_selected": cat_selected}
